4d4998e8c156bc3229c0c3f56a0ca003ffb1c080
John Ball <jlball@mit.edu>
2023-02-24 14:18:18 -0500

    Making sure all files are tracked

---
+++ openmc-scripts/arc-1/depletion_scan/pp-depletion-scan-arc-1.py
@@ -0,0 +1,177 @@
+import openmc
+from openmc.deplete import Results
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+from arc_nonproliferation.postprocess import *
+from arc_nonproliferation.constants import *
+from scipy.optimize import curve_fit
+from scipy.stats import linregress
+
+if sys.argv[1] is not None:
+    base_dir = './' + sys.argv[1]
+else:
+    raise ValueError("No base directory specified!")
+
+fusion_power = 500 #MW
+total_neutron_rate = fusion_power * neutrons_per_MJ
+
+# ====================================================
+# Time to a Significant Quantity
+# ====================================================
+
+""" Load masses and initialisze final output arrays """
+masses = np.loadtxt(base_dir + '/masses.txt')
+U_time_to_SQ = np.empty(len(masses))
+Th_time_to_SQ = np.empty(len(masses))
+
+""" Iterate through each mass simulated and compute time to SQ"""
+for i, mass in enumerate(masses):
+
+    """ Extract time to 1 SQ for Uranium """
+    os.chdir(base_dir + "/Uranium/" + str(mass))
+
+    U_results = Results('depletion_results.h5')
+    U_time_to_SQ[i] = extract_time_to_sq('U', U_results)
+
+    #While we're here, get the number of depletion steps:
+    time_steps = U_results.get_times()
+    num_steps = len(time_steps)
+
+    os.chdir("../../..")
+
+    """ Extract time to 1 SQ for Thorium """
+    os.chdir(base_dir + "/Thorium/" + str(mass))
+
+    Th_results = Results('depletion_results.h5')
+    Th_time_to_SQ[i] = extract_time_to_sq('Th', Th_results)
+
+    os.chdir("../../..")
+
+# ====================================================
+# Fission Power
+# ====================================================
+U_fission_powers = np.empty((len(masses), num_steps))
+Th_fission_powers = np.empty((len(masses), num_steps))
+
+for i, mass in enumerate(masses):
+    """ Uranium """
+    os.chdir(base_dir + "/Uranium/" + str(mass))
+
+    for step in range(0, num_steps):
+        sp = openmc.StatePoint('openmc_simulation_n'+str(step)+'.h5')
+        tally = sp.get_tally(name='FLiBe Tally')
+
+        U_fission_powers[i, step] = anp.get_uvalue(tally, 'kappa-fission').n * total_neutron_rate * MJ_per_eV
+
+    os.chdir('../../..')
+
+    """ Thorium """
+    os.chdir(base_dir + "/Thorium/" + str(mass))
+
+    for step in range(0, num_steps):
+        sp = openmc.StatePoint('openmc_simulation_n'+str(step)+'.h5')
+        tally = sp.get_tally(name='FLiBe Tally')
+
+        Th_fission_powers[i, step] = anp.get_uvalue(tally, 'kappa-fission').n * total_neutron_rate * MJ_per_eV
+
+    os.chdir('../../..')
+
+
+
+# ====================================================
+# Decay Heat
+# ====================================================
+
+
+# ====================================================
+# Plotting
+# ====================================================
+
+#Change into dedicated directory for figures or create figures directory
+try:
+    os.chdir(base_dir + "/figures")
+except:
+    os.mkdir(base_dir + "/figures")
+    os.chdir(base_dir + "/figures")
+
+""" Time to 1 Significant Quantity """
+fig, ax = plt.subplots()
+ax.spines["top"].set_color("None")
+ax.spines["right"].set_color("None")
+
+ax.scatter(masses/1e3, U_time_to_SQ/24, label="$^{238}$U", marker='o')
+ax.scatter(masses/1e3, Th_time_to_SQ/24, label="$^{232}$Th", marker='s')
+
+# Fit data to 1/x function:
+def fit(x, A, B):
+    return A/x + B
+
+U_popt, U_pcov = curve_fit(fit, masses, U_time_to_SQ)
+Th_popt, Th_pcov = curve_fit(fit, masses, Th_time_to_SQ)
+
+fit_masses = np.linspace(5, masses[-1], num=100)
+ax.plot(fit_masses/1e3, fit(fit_masses, *U_popt)/24, alpha=0.3)
+ax.plot(fit_masses/1e3, fit(fit_masses, *Th_popt)/24, alpha=0.3)
+
+ax.legend()
+
+ax.set_xlim(0, masses[-1]/1e3 + 2)
+ax.set_ylim(0, np.max(Th_time_to_SQ/24) + 5)
+
+ax.set_title("Time to Breed a Significant Quantity of Fissile Material", fontsize=14)
+ax.set_ylabel("Time (days)", fontsize=14)
+ax.set_xlabel("Mass of Fertile Material (metric tons)", fontsize=14)
+
+fig.savefig("time_to_sq.png")
+
+# Fissile Proliferance
+
+fusion_power = 500 #MW
+
+U_fissile_proliferance = 1/(U_time_to_SQ * masses/1e3 * fusion_power)
+Th_fissile_proliferance = 1/(Th_time_to_SQ * masses/1e3 * fusion_power)
+
+fig, ax = plt.subplots()
+
+ax.scatter(masses, U_fissile_proliferance)
+ax.scatter(masses, Th_fissile_proliferance)
+
+fig.savefig("fissile_proliferance.png")
+
+# Fission Power:
+fig, ax = plt.subplots()
+ax.spines["top"].set_color("None")
+ax.spines["right"].set_color("None")
+
+for i, mass in enumerate(masses):
+    """ Uranium """
+    ax.scatter(time_steps, U_fission_powers[i], c='r', s=2)
+
+    U_res = linregress(time_steps, U_fission_powers[i])
+
+    before_sq = np.array([0, U_time_to_SQ[i]/24])
+    after_sq = np.array([U_time_to_SQ[i]/24, time_steps[-1]])
+
+    ax.plot(before_sq, U_res.intercept + U_res.slope*before_sq, c='r', alpha=0.7)
+    ax.plot(after_sq, U_res.intercept + U_res.slope*after_sq, c='r', alpha=0.3)
+
+    """ Thorium """
+    ax.scatter(time_steps, Th_fission_powers[i], c='g', s=2)
+
+    Th_res = linregress(time_steps, Th_fission_powers[i])
+
+    before_sq = np.array([0, Th_time_to_SQ[i]/24])
+    after_sq = np.array([Th_time_to_SQ[i]/24, time_steps[-1]])
+
+    ax.plot(before_sq, Th_res.intercept + Th_res.slope*before_sq, c='g', alpha=0.7)
+    ax.plot(after_sq, Th_res.intercept + Th_res.slope*after_sq, c='g', alpha=0.3)
+
+ax.set_xlim(-5, 100)
+ax.set_ylim(0, 150)
+
+ax.set_title("Fission Power", fontsize=14)
+ax.set_ylabel("Fission Power (MW)", fontsize=14)
+ax.set_xlabel("Time (days)", fontsize=14)
+
+fig.savefig("fission_power.png")
