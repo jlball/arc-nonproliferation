bec05f5388d791c9ea0ff208974b2cacf14f84c7
John Ball <jlball@mit.edu>
2023-03-03 16:19:31 -0500

    Added Li enrichment depletion scan files

---
+++ openmc-scripts/arc-1/depletion_scan/depletion-scan-arc-1-Li6.py
@@ -0,0 +1,164 @@
+import arc_nonproliferation as anp
+import openmc
+import openmc.deplete
+import numpy as np
+import os
+import sys
+
+""" Handle command line arguments and setup directory structure """
+if sys.argv[1] is not None:
+    base_dir = str(sys.argv[1])
+    os.mkdir(base_dir)
+    print("OpenMC output saved to:", base_dir)
+
+    os.mkdir(base_dir + '/Uranium')
+    os.mkdir(base_dir + '/Thorium')
+
+def generate_device(dopant, dopant_mass, Li6_enrichment=7.5):
+    device = anp.Device()
+
+    # ==============================================================================
+    # Geometry
+    # ==============================================================================
+
+    """ PFCs and Vacuum Vessel """
+
+    vv_points = np.loadtxt("/home/jlball/arc-nonproliferation/data/arc_vv.txt")
+
+    pfc_polygon = openmc.model.Polygon(vv_points, basis='rz')
+    vv_inner_edge = pfc_polygon.offset(0.3) #PFC
+    vv_channel_inner = vv_inner_edge.offset(1.0) #VV
+    channel_outer = vv_channel_inner.offset(2.0) #FLiBe channels
+    vv_channel_outer = channel_outer.offset(3.0) #Channel shell
+
+    """ Blanket and Outer Blanket Tank """
+
+    blanket_points = np.loadtxt("/home/jlball/arc-nonproliferation/data/arc_blanket.txt")
+
+    blanket_inner = openmc.model.Polygon(blanket_points, basis='rz')
+    gap = blanket_inner.offset(1.0)
+    blanket_outer = gap.offset(2.0) #Blanket tank outer
+
+    regions = openmc.model.subdivide([pfc_polygon,
+                                    vv_inner_edge, vv_channel_inner,
+                                    channel_outer, vv_channel_outer,
+                                    blanket_inner, blanket_outer])
+
+    plasma, pfc, vv, channel, tank_inner, salt, tank_outer, outside = regions
+
+    doped_flibe = anp.doped_flibe(dopant, dopant_mass, volume=1e8, Li6_enrichment=Li6_enrichment)
+
+    device.plasma = openmc.Cell(region=plasma, fill=None, name='plasma')
+    device.pfc = openmc.Cell(region=pfc, fill=anp.tungsten, name='PFC')
+    device.vv = openmc.Cell(region=vv, fill=anp.vcrti_VV, name='VV')
+    device.channel = openmc.Cell(region=channel, fill=doped_flibe, name='channels')
+    device.tank_inner = openmc.Cell(region=tank_inner, fill=anp.vcrti_BI, name='tank inner')
+    device.blanket = openmc.Cell(region=salt, fill=doped_flibe, name='blanket')
+    device.tank_outer = openmc.Cell(region=tank_outer, fill=anp.vcrti_BO, name='tank outer')
+    device.domain.region = device.domain.region & outside
+
+    # ==============================================================================
+    # Settings
+    # ==============================================================================
+
+    """ Source Definition """
+    source = openmc.Source()
+    source.space = openmc.stats.CylindricalIndependent(openmc.stats.Discrete(475, 1), openmc.stats.Uniform(a=-np.pi/18, b=np.pi/18), openmc.stats.Discrete(0, 1))
+    source.angles = openmc.stats.Isotropic()
+    source.energy = openmc.stats.Discrete([14.1E6], [1.0])
+
+    device.settings.source = source
+
+    """ Run settings """
+    device.settings.photon_transport = False
+    device.settings.particles = int(1e5)
+    device.settings.batches = 10
+
+    # ==============================================================================
+    # Tallies
+    # ==============================================================================
+    """ Cylindrical Mesh Tally """
+    mesh = openmc.CylindricalMesh()
+    mesh.r_grid = np.linspace(25, 200, num=25)
+    mesh.z_grid = np.linspace(-200, 200, num=50)
+    mesh.phi_grid = np.array([0, (2 * np.pi)/(18 * 2)])
+    mesh_filter = openmc.MeshFilter(mesh)
+
+    device.add_tally('Mesh Tally', ['flux', '(n,Xt)', 'heating-local', 'absorption'], filters=[mesh_filter])
+    
+    """ FLiBe Tally """
+    flibe_filter = openmc.MaterialFilter(doped_flibe)
+    device.add_tally('FLiBe Tally', ['(n,Xt)', 'fission', 'kappa-fission', 'fission-q-prompt', 'fission-q-recoverable', 'heating', 'heating-local'], filters=[flibe_filter])
+    device.add_tally('Li Tally', ['(n,Xt)'], filters=[flibe_filter], nuclides=['Li6', 'Li7'])
+
+    return device
+
+# Plotting
+plot = openmc.Plot()
+plot.filename = 'geometry_plot'
+plot.basis = 'xz'
+plot.origin = (450, 0, 0)
+plot.width = (600, 600)
+plot.pixels = (2000, 2000)
+plot.color_by = 'cell'
+
+plots = openmc.Plots([plot])
+
+os.mkdir(base_dir + "/geometry_plots")
+os.chdir(base_dir + "/geometry_plots")
+
+# generte arbitrary device
+device = generate_device('U', 0)
+device.build()
+plots.export_to_xml()
+openmc.plot_geometry()
+
+os.chdir("../..")
+
+# ==============================================================================
+# Depletion Scan
+# ==============================================================================
+
+mass = 15e3
+
+enrichments = np.linspace(0, 100, num=6)
+np.savetxt(base_dir + '/enrichments.txt', enrichments)
+
+for enrichment in enrichments:
+    """ DEPLETION SETTINGS """
+    print("~~~~~~~~~~~~~~~~~~ ENRICHMENT: " + str(enrichment) + " kg ~~~~~~~~~~~~~~~~~~")
+
+    fusion_power = 500 #MW
+    num_steps = 8
+    time_steps = [100*24*60*60 / num_steps] * num_steps
+    source_rates = [fusion_power * anp.neutrons_per_MJ] * num_steps
+
+    chain_file = '/home/jlball/arc-nonproliferation/data/simple_chain_endfb71_pwr.xml'
+
+    """ Generate blankets doped to specified enrichment """
+    
+    U_device = generate_device("U", mass, Li6_enrichment=enrichment)
+    Th_device = generate_device("Th", mass, Li6_enrichment=enrichment)
+
+    """ Run depletion calculation """
+
+    # This is necessary to ensure the tally.xml file gets written into the directory where the depletion calculation will run
+    os.mkdir(base_dir + '/Uranium/'+ str(enrichment))
+    os.chdir(base_dir + '/Uranium/'+ str(enrichment))
+    U_device.build()
+    os.chdir('../../..')
+
+    U_device.deplete(time_steps, 
+        source_rates=source_rates, 
+        operator_kwargs={'chain_file':chain_file, 'normalization_mode':'source-rate'}, 
+        directory=base_dir + '/Uranium/'+ str(enrichment))
+
+    os.mkdir(base_dir + '/Thorium/'+ str(enrichment))
+    os.chdir(base_dir + '/Thorium/'+ str(enrichment))
+    Th_device.build()
+    os.chdir('../../..')
+
+    Th_device.deplete(time_steps, 
+        source_rates=source_rates, 
+        operator_kwargs={'chain_file':chain_file, 'normalization_mode':'source-rate'}, 
+        directory=base_dir + '/Thorium/' + str(enrichment))
---
+++ openmc-scripts/arc-1/depletion_scan/pp-depletion-scan-arc-1-Li6.py
@@ -0,0 +1,161 @@
+import openmc
+from openmc.deplete import Results
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+from arc_nonproliferation.postprocess import *
+from arc_nonproliferation.constants import *
+from scipy.optimize import curve_fit
+from scipy.stats import linregress
+
+if sys.argv[1] is not None:
+    base_dir = './' + sys.argv[1]
+else:
+    raise ValueError("No base directory specified!")
+
+fusion_power = 500 #MW
+total_neutron_rate = fusion_power * neutrons_per_MJ
+
+# ====================================================
+# Time to a Significant Quantity
+# ====================================================
+
+""" Load masses and initialisze final output arrays """
+enrichments = np.loadtxt(base_dir + '/enrichments.txt')
+U_time_to_SQ = np.empty(len(enrichments))
+Th_time_to_SQ = np.empty(len(enrichments))
+
+""" Iterate through each mass simulated and compute time to SQ"""
+for i, enrichment in enumerate(enrichments):
+
+    """ Extract time to 1 SQ for Uranium """
+    os.chdir(base_dir + "/Uranium/" + str(enrichment))
+
+    U_results = Results('depletion_results.h5')
+    U_time_to_SQ[i] = extract_time_to_sq('U', U_results)
+
+    #While we're here, get the number of depletion steps:
+    time_steps = U_results.get_times()
+    num_steps = len(time_steps)
+
+    os.chdir("../../..")
+
+    """ Extract time to 1 SQ for Thorium """
+    os.chdir(base_dir + "/Thorium/" + str(enrichment))
+
+    Th_results = Results('depletion_results.h5')
+    Th_time_to_SQ[i] = extract_time_to_sq('Th', Th_results)
+
+    os.chdir("../../..")
+
+# ====================================================
+# Fission Power
+# ====================================================
+U_fission_powers = np.empty((len(enrichments), num_steps, 2))
+Th_fission_powers = np.empty((len(enrichments), num_steps, 2))
+
+for i, enrichment in enumerate(enrichments):
+    """ Uranium """
+    os.chdir(base_dir + "/Uranium/" + str(enrichment))
+
+    for step in range(0, num_steps):
+        sp = openmc.StatePoint('openmc_simulation_n'+str(step)+'.h5')
+        tally = sp.get_tally(name='FLiBe Tally')
+
+        U_fission_powers[i, step, 0] = anp.get_uvalue(tally, 'kappa-fission').n * total_neutron_rate * MJ_per_eV
+        U_fission_powers[i, step, 1] = anp.get_uvalue(tally, 'kappa-fission').s * total_neutron_rate * MJ_per_eV
+
+    os.chdir('../../..')
+
+    """ Thorium """
+    os.chdir(base_dir + "/Thorium/" + str(enrichment))
+
+    for step in range(0, num_steps):
+        sp = openmc.StatePoint('openmc_simulation_n'+str(step)+'.h5')
+        tally = sp.get_tally(name='FLiBe Tally')
+
+        Th_fission_powers[i, step, 0] = anp.get_uvalue(tally, 'kappa-fission').n * total_neutron_rate * MJ_per_eV
+        Th_fission_powers[i, step, 1] = anp.get_uvalue(tally, 'kappa-fission').s * total_neutron_rate * MJ_per_eV
+
+    os.chdir('../../..')
+
+
+
+# ====================================================
+# Decay Heat
+# ====================================================
+
+
+# ====================================================
+# Plotting
+# ====================================================
+
+#Change into dedicated directory for figures or create figures directory
+try:
+    os.chdir(base_dir + "/figures")
+except:
+    os.mkdir(base_dir + "/figures")
+    os.chdir(base_dir + "/figures")
+
+""" Time to 1 Significant Quantity """
+fig, ax = plt.subplots()
+ax.spines["top"].set_color("None")
+ax.spines["right"].set_color("None")
+
+ax.scatter(enrichments, U_time_to_SQ/24, label="$^{238}$U", marker='o')
+ax.scatter(enrichments, Th_time_to_SQ/24, label="$^{232}$Th", marker='s')
+
+np.save("U_time_to_SQ_depletion", U_time_to_SQ)
+np.save("Th_time_to_SQ_depletion", Th_time_to_SQ)
+
+ax.legend()
+
+ax.set_xlim(0, enrichments[-1] + 2)
+ax.set_ylim(0, np.max(Th_time_to_SQ/24) + 5)
+
+ax.set_title("Time to Breed a Significant Quantity of Fissile Material", fontsize=14)
+ax.set_ylabel("Time (days)", fontsize=14)
+ax.set_xlabel("Li6 Enrichment", fontsize=14)
+
+fig.savefig("time_to_sq.png")
+
+# Fission Power:
+fig, ax = plt.subplots()
+ax.spines["top"].set_color("None")
+ax.spines["right"].set_color("None")
+
+U_fission_power_at_SQ = np.empty((len(enrichments)))
+Th_fission_power_at_SQ = np.empty((len(enrichments)))
+for i, mass in enumerate(enrichments):
+    """ Uranium """
+    U_res = linregress(time_steps, U_fission_powers[i, :, 0])
+    U_fission_power_at_SQ[i] = U_res.intercept + U_res.slope*U_time_to_SQ[i]
+
+    """ Thorium """
+    Th_res = linregress(time_steps, Th_fission_powers[i, :, 0])
+    Th_fission_power_at_SQ[i] = Th_res.intercept + Th_res.slope*Th_time_to_SQ[i]
+
+ax.scatter(enrichments, U_fission_powers[:, 0, 0], c='r', s=4, label='At t = 0')
+ax.scatter(enrichments, Th_fission_powers[:, 0, 0], c='g', s=4, label='At t = 0')
+
+ax.scatter(enrichments, U_fission_power_at_SQ, c='r', s=4, label='After 1 SQ bred')
+ax.scatter(enrichments, Th_fission_power_at_SQ, c='g', s=4, label='After 1 SQ bred')
+
+ax.fill_between(enrichments, U_fission_powers[:, 0, 0], U_fission_power_at_SQ, color='r', alpha=0.3)
+ax.fill_between(enrichments, Th_fission_powers[:, 0, 0], Th_fission_power_at_SQ, color='g', alpha=0.3)
+
+text_offset = 5
+ax.annotate("t = $t_{SQ}$", (enrichments[-1], U_fission_power_at_SQ[-1]), color='r', textcoords='offset points', xytext=(text_offset, 0))
+ax.annotate("t = 0", (enrichments[-1], U_fission_powers[-1, 0, 0]), color='r', textcoords='offset points', xytext=(text_offset, 0))
+
+ax.annotate("t = $t_{SQ}$", (enrichments[-1], Th_fission_power_at_SQ[-1]), color='g', textcoords='offset points', xytext=(text_offset, 0))
+ax.annotate("t = 0", (enrichments[-1], Th_fission_powers[-1, 0, 0]), color='g', textcoords='offset points', xytext=(text_offset, 0))
+
+ax.set_xlim(0, enrichments[-1] + 5)
+ax.set_ylim(0, U_fission_power_at_SQ[-1] + 10)
+
+ax.set_title("Fission Power in Doped FLiBe Blanket", fontsize=14)
+ax.set_ylabel("Fission Power (MW)", fontsize=14)
+ax.set_xlabel("Li6 Enrichment", fontsize=14)
+
+fig.savefig("fission_power.png")
