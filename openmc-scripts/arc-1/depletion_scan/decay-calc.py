ee0f40f3a4a7b8897ce9c13e13c80ac2b620451f
John Ball <jlball@mit.edu>
2024-02-14 09:18:18 -0500

    Basic cooldown calc now functioning

--- openmc-scripts/arc-1/depletion_scan/decay-calc.py
+++ openmc-scripts/arc-1/depletion_scan/decay-calc.py
@@ -1,12 +1,19 @@
 import openmc
-from openmc.deplete import Results, IndependentOperator
+from openmc.deplete import Results, IndependentOperator, PredictorIntegrator
 import sys
 import os
 import numpy as np
 import pickle
 from arc_nonproliferation.postprocess import *
 from arc_nonproliferation.device import *
+from arc_nonproliferation.constants import chain_file
 
+cooldow_folder_name = "dose_rate_cooldown"
+
+decay_time_steps = np.logspace(-2, 4, num=50) #days
+source_rates = np.zeros(len(decay_time_steps))
+
+openmc.config['chain_file'] = chain_file
 
 if sys.argv[1] is not None:
     base_dir = './' + sys.argv[1]
@@ -31,17 +38,40 @@ for i, mass in enumerate(masses):
 
     results = Results("depletion_results.h5")
 
+    # Linearly interpolate material compositions to t_SQ
     blanket_mat = lin_interp_material(results, "doped flibe blanket", U_time_to_sq[i])
     channel_mat = lin_interp_material(results, "doped flibe channels", U_time_to_sq[i])
 
-    # Perform depletion calc
+    # Create or enter subdirectory for decay calc
+    try:
+        os.chdir(cooldow_folder_name)
+    except:
+        os.mkdir(cooldow_folder_name)
+        os.chdir(cooldow_folder_name)
+
+    # Perform decay only depletion calc
 
     device = generate_device("U", 0, Li6_enrichment=7.5)
 
+    # Set new material volumes
+    channel_mat.volume = device.channel.fill.volume
+    blanket_mat.volume = device.channel.fill.volume
+
+    device.channel.fill = channel_mat
+    device.blanket.fill = blanket_mat
+
+    device.build()
+
     operator = IndependentOperator(openmc.Materials([device.channel.fill, device.blanket.fill]),
-                                    fluxes=[0, 0],
-                                    micros=[openmc.deplete.MicroXS(), openmc.deplete.MicroXS()],
+                                    fluxes=np.zeros(2),
+                                    micros=[openmc.deplete.MicroXS(np.zeros((0,0)), [], []), openmc.deplete.MicroXS(np.zeros((0,0)), [], [])],
                                     normalization_mode="source-rate")
 
-    os.chdir("..")
+    integrator = PredictorIntegrator(operator, 
+                                        decay_time_steps, 
+                                        source_rates=source_rates,
+                                        timestep_units='d')
+    
+    integrator.integrate()
 
+    os.chdir("../..")
