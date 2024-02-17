8d0d0213badc72fd0cd1fd7adee5541eb4f5c40d
John Ball <jlball@mit.edu>
2024-02-17 14:58:54 -0500

    Switched to using multiprocess.Pool() instead of Process() to make it easier to parallelize across all cores

--- openmc-scripts/arc-1/depletion_scan/pp-decay-calc.py
+++ openmc-scripts/arc-1/depletion_scan/pp-decay-calc.py
@@ -7,7 +7,7 @@ from arc_nonproliferation.postprocess import *
 from arc_nonproliferation.constants import *
 from progressbar import progressbar
 import pickle
-from multiprocessing import Process
+import multiprocessing as mp
 
 cooldow_folder_name = "dose_rate_cooldown"
 num_timesteps = 51
@@ -25,20 +25,6 @@ else:
 
 masses = np.loadtxt(base_dir + "/masses.txt")
 
-
-def parallel_extract_dose_rate(all_mats, material_name, time_steps, output_variable):
-    dose_rates = np.empty(len(time_steps))
-
-    for i, time in enumerate(time_steps):
-        print(f"Timestep: {i}")
-        mats = all_mats[i]
-
-        blanket_mat = get_material_by_name(mats, material_name)
-
-        dose_rates[i] = extract_contact_dose_rate(blanket_mat)
-
-    output_variable = dose_rates
-
 dose_rate_dict = {}
 
 if __name__ == "__main__":
@@ -47,53 +33,47 @@ if __name__ == "__main__":
 
         U_dose_rates, Th_dose_rates = np.zeros((len(masses), num_timesteps)), np.zeros((len(masses), num_timesteps))
 
+        pool = mp.Pool()
+
+        U_blanket_args = []
+        U_channel_args = []
+
         for i, mass in enumerate(masses):
             print(f"~~~~~~ MASS: {mass} ~~~~~~~")
 
+            # Get blanket and channel matertials at each timestep for Uranium
             os.chdir(f"Uranium/{mass}/{cooldow_folder_name}")
             U_results = Results("depletion_results.h5")
             time_steps = U_results.get_times()
 
-            U_mats = []
+            U_blanket_mats = []
+            U_channel_mats = []
             for j, time in enumerate(time_steps):
-                U_mats.append(U_results.export_to_materials(j))
+                U_blanket_mats.append(get_material_by_name(U_results.export_to_materials(j), "doped flibe blanket"))
+                U_channel_mats.append(get_material_by_name(U_results.export_to_materials(j), "doped flibe channels"))
             os.chdir("../../..")
 
+            # Get blanket and channel matertials at each timestep for Thorium
             os.chdir(f"Thorium/{mass}/{cooldow_folder_name}")
             Th_results = Results("depletion_results.h5")
 
-            Th_mats = []
+            Th_blanket_mats = []
+            Th_channel_mats = []
             for j, time in enumerate(time_steps):
-                Th_mats.append(Th_results.export_to_materials(j))
+                Th_blanket_mats.append(get_material_by_name(Th_results.export_to_materials(j), "doped flibe blanket"))
+                Th_channel_mats.append(get_material_by_name(Th_results.export_to_materials(j), "doped flibe channels"))
             os.chdir("../../..")
 
-            U_blanket_dose_rates, Th_blanket_dose_rates = 0, 0
-            U_channel_dose_rates, Th_channel_dose_rates = 0, 0
+            # Dispatch computation of contact dose rates to process pool for speed
+            U_blanket_result = pool.map_async(extract_contact_dose_rate, U_blanket_mats)
+            U_channel_result = pool.map_async(extract_contact_dose_rate, U_channel_mats)
 
-            U_blanket_proc = Process(target=parallel_extract_dose_rate, 
-                                    args=(U_mats, "doped flibe blanket", time_steps, U_blanket_dose_rates))
-            U_channel_proc = Process(target=parallel_extract_dose_rate, 
-                                   args=(U_mats, "doped flibe channels", time_steps, U_channel_dose_rates))
-            
-            Th_blanket_proc = Process(target=parallel_extract_dose_rate, 
-                                    args=(Th_mats, "doped flibe blanket", time_steps, Th_blanket_dose_rates))
-            Th_channel_proc = Process(target=parallel_extract_dose_rate, 
-                                    args=(Th_mats, "doped flibe channels", time_steps, Th_channel_dose_rates))
+            Th_blanket_result = pool.map_async(extract_contact_dose_rate, Th_blanket_mats)
+            Th_channel_result = pool.map_async(extract_contact_dose_rate, Th_channel_mats)
 
-            # Start all processes
-            U_blanket_proc.start()
-            U_channel_proc.start()
-            Th_blanket_proc.start()
-            Th_channel_proc.start()
-
-            # Wait for all processes to finish before continuing
-            U_blanket_proc.join()
-            U_channel_proc.join()
-            Th_blanket_proc.join()
-            Th_channel_proc.join()
-
-            U_dose_rates[i] = U_blanket_dose_rates + U_channel_dose_rates
-            Th_dose_rates[i] = Th_blanket_dose_rates + Th_channel_dose_rates
+            # Sum results element-wise and store in 2D array
+            U_dose_rates[i] = np.asarray(U_blanket_result.get()) + np.asarray(U_channel_result.get())
+            Th_dose_rates[i] = np.asarray(Th_blanket_result.get()) + np.asarray(Th_channel_result.get())
 
         dose_rate_dict["U"] = U_dose_rates
         dose_rate_dict["Th"] = Th_dose_rates
@@ -113,32 +93,36 @@ if __name__ == "__main__":
         with open(f"{base_dir}/data/dose_rate_dict.pkl", 'rb') as file:
             dose_rate_dict = pickle.load(file)
 
-        print(dose_rate_dict)
-        try:
-            os.chdir("figures")
-        except:
-            os.mkdir("figures")
-            os.chdir("figures")
+    try:
+        os.chdir("figures")
+    except:
+        os.mkdir("figures")
+        os.chdir("figures")
 
-        for dopant in dopants:
-            dose_rates = dose_rate_dict[dopant]
-            time_steps = dose_rate_dict["time_steps"]
+    for dopant in dopants:
+        dose_rates = dose_rate_dict[dopant]
+        time_steps = dose_rate_dict["time_steps"]
 
-            fig, ax = plt.subplots()
+        fig, ax = plt.subplots()
 
-            for i, dose_rate in enumerate(dose_rates):
-                ax.plot(time_steps, dose_rates[i])
+        for i, dose_rate in enumerate(dose_rates):
+            ax.plot(time_steps, dose_rates[i])
 
-            ax.set_xlabel("Time (days)")
-            ax.set_ylabel("Contact Dose Rate (Sv/hr)")
-            ax.set_title(dopant)
+        ax.set_xlabel("Time (days)")
+        ax.set_ylabel("Contact Dose Rate (Sv/hr)")
 
-            ax.set_xscale("log")
+        if dopant == "U":
+            ax.set_title("Decay of contact dose rate in Uranium fueled FLiBe")
 
-            fig.savefig(f"{dopant}_contact_dose_rate_decay.png", dpi=300)
-            print(f"Saved {dopant} fig")
+        if dopant == "Th":
+            ax.set_title("Decay of contact dose rate in Thorium fueled FLiBe")
 
-        os.chdir("../..")
+        ax.set_xscale("log")
+
+        fig.savefig(f"{dopant}_contact_dose_rate_decay.png", dpi=300)
+        print(f"Saved {dopant} fig")
+
+    os.chdir("../..")
 
 
 
